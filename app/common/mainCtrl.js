eca6fc6d057e1a63ddcbef7fbc7fa4759d195e16
kiran paturi <kpaturi@smarttuition.com>
2016-08-04 12:40:48 +0530

    Added code

--- app/common/mainCtrl.js
+++ app/common/mainCtrl.js
@@ -1,10 +1,25 @@
 angular.module("flipzon")
-    .controller("mainCtrl", ["$scope", function($scope) {
+    .controller("mainCtrl", ["$scope", "loginSvc", function ($scope, loginSvc) {
         $scope.headerUrl = "app/templates/header.html";
+        $scope.showLogin = true;
 
         var baseUrl = "app/templates/";
-        $scope.loadContent = function(type) {
-            $scope.contentUrl = baseUrl + type + ".html";
+        $scope.loadContent = function (type) {
+            var security = loginSvc.isAuthenticated();
+
+            if (security.authenticated) {
+                $scope.showLogin = false;
+                $scope.userdetails = security.firstname + "," + security.lastname;
+                $scope.contentUrl = baseUrl + type + ".html";
+            } else {
+                $scope.showLogin = true;
+                $scope.contentUrl = baseUrl + "login.html";
+            }
 
         };
-    }]);
+        $scope.logout = function () {
+            loginSvc.logout();
+            $scope.loadContent("login");
+        }
+
+            }]);
--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -1,5 +1,10 @@
 angular.module("login")
-    .controller("loginCtrl", ["$scope", function ($scope) {
-        $scope.loginUser = {};
+    .controller("loginCtrl", ["$scope", "loginSvc",
+                              function ($scope, loginSvc) {
+            $scope.loginUser = {};
+
+            $scope.login = function () {
+                loginSvc.loginUser($scope.loginUser);
+            };
 
 }]);
