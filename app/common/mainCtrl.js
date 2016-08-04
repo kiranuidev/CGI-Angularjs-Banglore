e9ddfda1107c14028e8b47bd6d50f5f5d31e730d
kiran paturi <kpaturi@smarttuition.com>
2016-08-04 16:39:14 +0530

    Added code

--- app/common/mainCtrl.js
+++ app/common/mainCtrl.js
@@ -5,15 +5,19 @@ angular.module("flipzon")
 
         var baseUrl = "app/templates/";
         $scope.loadContent = function (type) {
-            var security = loginSvc.isAuthenticated();
-
-            if (security.authenticated) {
-                $scope.showLogin = false;
-                $scope.userdetails = security.firstname + "," + security.lastname;
+            if (type == "register") {
                 $scope.contentUrl = baseUrl + type + ".html";
             } else {
-                $scope.showLogin = true;
-                $scope.contentUrl = baseUrl + "login.html";
+                var security = loginSvc.isAuthenticated();
+
+                if (security.authenticated) {
+                    $scope.showLogin = false;
+                    $scope.userdetails = security.firstname + "," + security.lastname;
+                    $scope.contentUrl = baseUrl + type + ".html";
+                } else {
+                    $scope.showLogin = true;
+                    $scope.contentUrl = baseUrl + "login.html";
+                }
             }
 
         };
--- app/components/components.js
+++ app/components/components.js
@@ -10,3 +10,46 @@ angular.module("components")
         };
 
 }]);
+
+angular.module("components")
+    .directive("cgiHeader", [function () {
+
+        return {
+            templateUrl: "app/templates/header.html",
+            restrict: "AEC"
+        };
+
+}]);
+
+
+
+angular.module("components")
+    .directive("cgiNumbersOnly", [function () {
+
+        return {
+            restrict: "A",
+            link: function (scope, element, attrs) {
+                /*console.log(scope);
+                console.log(element);
+                console.log(attrs);*/
+
+                element.bind("keypress", function (evt) {
+                    var keycode = evt.keyCode;
+                    var length = attrs["inputlength"];
+                    var data = this.value.length;
+                    var reg = /^\d+$/;
+
+                    var exp = new RegExp(reg);
+
+                    if (!exp.test(evt.key)) {
+                        evt.preventDefault();
+                    }
+                    /*if (data < length && (keycode >= 48 && keycode <= 57)) {
+
+} else {
+    evt.preventDefault();
+}*/
+                });
+            }
+        };
+}]);
