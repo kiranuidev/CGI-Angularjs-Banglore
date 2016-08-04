5e6b0387de24dc77ec3ab683ec13fb2d532111af
kiran paturi <kpaturi@smarttuition.com>
2016-08-04 13:26:13 +0530

    Added code

--- app/products/productCtrl.js
+++ app/products/productCtrl.js
@@ -2,9 +2,19 @@ angular.module("products")
     .controller("productCtrl", ["$scope", "productSvc",
                                 function ($scope, productSvc) {
 
-            $scope.products = productSvc.products();
+            //$scope.products = productSvc.products();
 
             $scope.addProduct = function (product) {
                 productSvc.addProductForCheckout(product);
-            }
+            };
+
+            productSvc.getProductsFromPromise()
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (response) {
+                    console.log(response);
+                });
+            console.log("I am in the regularmode");
+
 }]);
--- app/products/productSvc.js
+++ app/products/productSvc.js
@@ -1,24 +1,53 @@
 angular.module("products")
-    .service("productSvc", ["$http", function ($http) {
-        this.products = function () {
-            $http.get("app/data/products.json")
-                .then(function (response) {
-                    console.log(response);
-                }).catch(function (response) {
-                    console.log(response);
-                });
-        };
+    .service("productSvc", ["$http", "$q",
+                            function ($http, $q) {
+            this.products = function () {
+                $http.get("app/data/products.json")
+                    .then(function (response) {
+                        console.log(response);
+                    }).catch(function (response) {
+                        console.log(response);
+                    });
+            };
 
-        var productsForCheckout = [];
+            var productsForCheckout = [];
 
+            this.getProductsFromPromise = function () {
+                var dfd = $q.defer();
 
-        this.addProductForCheckout = function (item) {
-            productsForCheckout.push(item);
+                var items = [
+                    {
+                        "id": "1",
+                        "model": "Tom",
+                        "name": "Cruise",
+                        "description": "AAAA",
+                        "price": 2000
+      },
+                    {
+                        "id": "1",
+                        "model": "KIN",
+                        "name": "JOO",
+                        "description": "abbbb",
+                        "price": 2000
+      }];
 
-        };
+                if (items) {
+                    dfd.resolve(items)
+                } else {
+                    dfd.reject("there are no items");
+                }
 
-        this.getSelectedProducts = function () {
-            return productsForCheckout;
-        };
+                return dfd.promise;
+
+            };
+
+            this.addProductForCheckout = function (item) {
+                productsForCheckout.push(item);
+
+            };
+
+            this.getSelectedProducts = function () {
+                return productsForCheckout;
+            };
 
 }]);
